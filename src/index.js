14c4f844c9a8e6a9b04b00d9b2feff18024e62dc
Mijail <mijail.iso@ciencias.unam.mx>
2021-03-14 23:34:26 -0600

    create components lesson

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
+var element = <button className="btn btn-danger">Hello World</button>;
+ReactDOM.render(element, document.getElementById("root"));
+console.log(element);
+console.log("HOLA mundo");
+console.log("HOLA mundo MIJAIL");
